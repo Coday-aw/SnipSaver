6ba7c44876804ea906e47faf12a608199251abc2
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2025-05-20 10:13:31 +0200

    commit

--- app/components/sidebar/CloseSideBar.tsx
+++ app/components/sidebar/CloseSideBar.tsx
@@ -4,12 +4,14 @@ import { IoIosHeart } from "react-icons/io";
 import { IoMdCreate } from "react-icons/io";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
+import { useActiveLink } from "../context/ActiveLinkContext";
 
 interface CloseSideBarProps {
   onClick: () => void;
 }
 
 const CloseSideBar = ({ onClick }: CloseSideBarProps) => {
+  const { activeLink, setActiveLink } = useActiveLink();
   return (
     <div className="flex flex-col  gap-10 border  border-slate-300 dark:bg-slate-800 dark:border-none shadow-lg w-14 h-screen p-7 rounded-lg">
       <div className="flex flex-col items-center gap-10">
@@ -24,7 +26,12 @@ const CloseSideBar = ({ onClick }: CloseSideBarProps) => {
         </button>
 
         <div className=" flex flex-col gap-10">
-          <div className=" relative group hover:bg-blue-500 p-2 hover:text-white  rounded-lg cursor-pointer">
+          <div
+            className={` relative group hover:bg-blue-500 p-2 hover:text-white  rounded-lg cursor-pointer ${
+              activeLink === "/create" ? "bg-blue-500 text-white" : ""
+            }`}
+            onClick={() => setActiveLink("/create")}
+          >
             <span className=" absolute top-[-34px] text-center right-[-6px] text-white bg-black p-1 text-sm rounded-full opacity-0 group-hover:opacity-100 ">
               Create
             </span>
@@ -32,7 +39,12 @@ const CloseSideBar = ({ onClick }: CloseSideBarProps) => {
               <IoMdCreate size={25} />
             </Link>
           </div>
-          <div className=" relative group hover:bg-blue-500 p-2 hover:text-white  rounded-lg cursor-pointer">
+          <div
+            className={`relative group hover:bg-blue-500 p-2 hover:text-white  rounded-lg cursor-pointer ${
+              activeLink === "/dashboard" ? "bg-blue-500 text-white" : ""
+            } `}
+            onClick={() => setActiveLink("/dashboard")}
+          >
             <span className=" absolute top-[-34px] text-center right-[-9px] text-white bg-black p-1 text-sm rounded-full opacity-0 group-hover:opacity-100 ">
               Snippets
             </span>
@@ -40,7 +52,12 @@ const CloseSideBar = ({ onClick }: CloseSideBarProps) => {
               <AiFillAppstore size={25} />
             </Link>
           </div>
-          <div className=" relative group hover:bg-blue-500 p-2 hover:text-red-500  rounded-lg cursor-pointer">
+          <div
+            className={`relative group hover:bg-blue-500 p-2 hover:text-red-500  rounded-lg cursor-pointer ${
+              activeLink === "/favorits" ? "bg-blue-500 text-red-500" : ""
+            }`}
+            onClick={() => setActiveLink("/favorits")}
+          >
             <span className=" absolute top-[-34px] text-center right-[-8px] text-white bg-black p-1 text-sm rounded-full opacity-0 group-hover:opacity-100 ">
               Favorits
             </span>
